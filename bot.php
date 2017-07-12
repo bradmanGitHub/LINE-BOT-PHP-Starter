7578aa1859f2708cf6099b24300b218aabfd27f9
mongkonchai.k <mongkonchai.k@K16SRVG9BRSF2.kasikornbank.com>
2017-07-12 07:26:29 +0700

    ้

--- bot.php
+++ bot.php
@@ -19,8 +19,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				//'text' => $text . ' mk '
-				'text' => $text
+				'text' => $text . ' ÊÅÑ´ '
+				//'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
