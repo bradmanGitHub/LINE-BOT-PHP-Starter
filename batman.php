35f269c84471a8c8a788727abc032a2c7f3951d9
bradman <mongkonchai_kk@hotmail.com>
2017-08-02 20:23:11 +0700

    dd

--- batman.php
+++ batman.php
@@ -1,15 +1,16 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+
 use Wit\Wit;
 $app = new Wit(array(
-    'default_access_token' => 'VTNKPHFNIEYJVKPR3QPVXU3XG43PDHYK') 
+    'default_access_token' => 'VTNKPHFNIEYJVKPR3QPVXU3XG43PDHYK')
 );
 
 
 //--invoke wit.ai --------------- START
-//$response = $app->get('/intents');
-//var_dump($response->getDecodedBody());
+$response = $app->get('/intents');
+var_dump($response->getDecodedBody());
 
 $data = [
     "name" => "food_request",
@@ -27,6 +28,4 @@ var_dump($response->getDecodedBody());
 //$messageStr = $response->getDecodedBody();
 //--invoke wit.ai --------------- END
 
-
-		
 echo "OKkk";
