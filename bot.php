00301e58ff473d4427dc7f83a60a8f424646be3b
bradman <mongkonchai_kk@hotmail.com>
2017-08-02 11:45:57 +0700

    test kkk

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' =>  'OK Boss..'. $text
-				//'text' => $text
+				//'text' => $text //kk
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
