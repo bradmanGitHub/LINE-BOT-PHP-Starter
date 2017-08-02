d66fef3397b27f37eca0727f7b3a1d33dcf99ae3
bradman <mongkonchai_kk@hotmail.com>
2017-08-02 20:16:27 +0700

    dd

---
+++ batman.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+use Wit\Wit;
+$app = new Wit(array(
+    'default_access_token' => 'VTNKPHFNIEYJVKPR3QPVXU3XG43PDHYK')
+);
+
+
+//--invoke wit.ai --------------- START
+//$response = $app->get('/intents');
+//var_dump($response->getDecodedBody());
+
+$data = [
+    "name" => "food_request",
+    "doc"  => "detect food request",
+    "expressions" => [
+        ["body" => $text]
+        //,
+        //["body" => "I want to fly from london to sfo"],
+        //["body" => "need a flight from paris to tokyo"],
+    ]
+];
+
+$response = $app->post('/intents', $data);
+//$messageStr = var_dump($response->getDecodedBody());
+//$messageStr = $response->getDecodedBody();
+//--invoke wit.ai --------------- END
+
+
+		
+echo "OK";
